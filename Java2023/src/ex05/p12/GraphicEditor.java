dc1990b25d1bf0f84cdb90fc22cf27a4d89f2f15
admin <admin@DESKTOP-BSVD8GJ>
2023-03-03 10:21:41 +0900

    5장 연습문제 마무리

--- Java2023/src/ex05/p12/GraphicEditor.java
+++ Java2023/src/ex05/p12/GraphicEditor.java
@@ -86,20 +86,20 @@ public class GraphicEditor
 		if(start == null)
 			return;
 		
-		if(start == last)
-		{
-			start = null;
-			last = null;
-			return;
-		}
-		
-		for(int i=0;i<postion;i++)
+		for(int i=1;i<postion;i++)
 		{
 			previous = current;
 			current = current.getNext();
 			
 			if(current == null) //마지막이면
 				return;
+		}
+		
+		if(start == last)
+		{
+			start = null;
+			last = null;
+			return;
 		}
 		
 		if(current == start)
